8663fc03fee6cb9498eb7faaf34e0f99638ae837
dldkfka331 <56900372+dldkfka331@users.noreply.github.com>
2020-02-21 11:42:39 +0900

    2020.02.21

--- assets/js/custom5.js
+++ assets/js/custom5.js
@@ -1,5 +1,17 @@
 $(function(){
 
+    //skrollr
+    var s = skrollr.init(
+        smoothScrolling = true
+    );
+    
+    //메인 화살표
+    setInterval(down,1000);
+    function down(){
+        $(".main .main_row").animate({bottom : "5%"},500, 'easeInCubic');
+        $(".main .main_row").animate({bottom : "8%"},500,'easeOutCubic');
+    }
+    
     
     $(".main .main_box .mb_text").mousemove(function(){
         $(this).css("display","none")
