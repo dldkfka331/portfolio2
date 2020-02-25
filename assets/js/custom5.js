b1dc97bbd9415e1cdef8d3520d604d6e97b8971f
dldkfka331 <56900372+dldkfka331@users.noreply.github.com>
2020-02-25 18:04:24 +0900

    2020.02.25

--- assets/js/custom5.js
+++ assets/js/custom5.js
@@ -10,12 +10,36 @@ $(function(){
     function down(){
         $(".main .main_row").animate({bottom : "5%"},500, 'easeInCubic');
         $(".main .main_row").animate({bottom : "8%"},500,'easeOutCubic');
-    }
-    
-    
+    };
+
+    //메인 텍스트 지워지기    
     $(".main .main_box .mb_text").mousemove(function(){
         $(this).css("display","none")
+    }); 
+
+    //햄버거메뉴
+    $(".ham").click(function(){
+        $(this).toggleClass("click");
+        $(".nav").toggleClass("click");
     });
 
+   
 
+
+    //소개부분 클릭이미지 반짝이기
+    setInterval(click, 500);
+    function click(){
+        $(".click_alerm01").toggleClass("none");
+    };
+      
+     //소개부분 이미지 생성
+    $(".who_text_about .who_click").find("span").click(function(){
+        let target =$(this);
+        let index =target.index();
+        let cookies = $(".who_click_img > div").eq(index);
+        cookies.css("display" , "block");
+    });
+
+    
+    
 });
